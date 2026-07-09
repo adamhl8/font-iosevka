0650d8774a5b07ea7871da130918eecf42bf2096
Adam Langbert <adamhl@pm.me>
2026-07-08 21:27:12 -0500

    ci: fix release workflow

--- knip.ts
+++ knip.ts
@@ -1,5 +1,5 @@
 import { knipConfig } from "@adamhl8/configs"
 
-const config = knipConfig({ entry: ["src/index.ts"], ignoreBinaries: ["just", "fontforge"] }, { arrays: "replace" })
+const config = knipConfig({ entry: [], ignoreBinaries: ["just", "fontforge"] }, { arrays: "replace" })
 
 export default config
