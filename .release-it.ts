5ce196c2cc166b083f4f587ed3c59a74beae8f59
Adam Langbert <adamhl@pm.me>
2026-07-12 00:37:17 -0500

    ci: simplify release process

--- .release-it.ts
+++ .release-it.ts
@@ -1,5 +1,12 @@
 import { releaseItConfig } from "@adamhl8/configs"
 
-const config = releaseItConfig()
+const config = releaseItConfig({
+  hooks: {
+    "after:bump": ["just build-fonts"],
+  },
+  github: {
+    assets: ["out/*.ttf"],
+  },
+})
 
 export default config
--- knip.ts
+++ knip.ts
@@ -1,5 +1,5 @@
 import { knipConfig } from "@adamhl8/configs"
 
-const config = knipConfig({ ignoreBinaries: ["just", "fontforge"] }, { arrays: "replace" })
+const config = knipConfig({ ignoreBinaries: ["fontforge"] })
 
 export default config
